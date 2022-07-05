9ac552fbe7df7ab40e33249408d4ae7d33323a6b
Lisaag <lisatgroen@hotmail.com>
2022-07-05 10:44:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+function App(){
+    const title = 'Blog Post'
+    const body = 'This is my blog post'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'},
+    ]
+
+    const showComments = true;
+
+    const commentBlock = (<div className="comments">
+            <h3>Comments({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>)
+
+    return(
+        <div className='container'>
+            <h1>{title.toUpperCase()}</h1>
+            <p>{body}</p>
+
+            {showComments && commentBlock }
+        </div>
+    ) 
 }
 
-export default App;
+export default App
