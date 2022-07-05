9847ae8ba3a89ba94b622786e5eb4a317ad39229
Lisaag <lisatgroen@hotmail.com>
2022-07-05 14:16:59 +0900

    component level state

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
-function App(){
-    const title = 'Blog Post'
-    const body = 'This is my blog post'
+import Header from './components/Header';
+import FeedbackItem from './components/FeedbackItem';
 
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'},
-    ]
-
-    const showComments = true;
-
-    const commentBlock = (<div className="comments">
-            <h3>Comments({comments.length})</h3>
-            <ul>
-                {comments.map((comment, index) => (
-                <li key={index}>{comment.text}</li>
-                ))}
-            </ul>
-        </div>)
-
-    return(
-        <div className='container'>
-            <h1>{title.toUpperCase()}</h1>
-            <p>{body}</p>
-
-            {showComments && commentBlock }
-        </div>
-    ) 
+function App() {
+	return (
+		<>
+			<Header></Header>
+			<div className='container'>
+				<FeedbackItem />
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;
