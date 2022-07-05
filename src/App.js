f5542056f63cd13760e875d0403426b858cc71f5
Lisaag <lisatgroen@hotmail.com>
2022-07-05 14:41:36 +0900

    Managing global state
    section 15

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Header from './components/Header';
-import FeedbackItem from './components/FeedbackItem';
+import { useState } from 'react'
+import Header from './components/Header'
+import FeedbackData from './data/FeedbackData'
+import FeedbackList from './components/FeedbackList'
 
 function App() {
+	const [feedback, setFeedback] = useState(FeedbackData)
+
 	return (
 		<>
 			<Header></Header>
 			<div className='container'>
-				<FeedbackItem />
+				<FeedbackList feedback={feedback} />
 			</div>
 		</>
-	);
+	)
 }
 
-export default App;
+export default App
